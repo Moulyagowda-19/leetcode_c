26ed63169153d0d23023a6a7e1bd34f31d06021a
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-03-12 21:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (5.32%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.c
@@ -0,0 +1,14 @@
+int subtractProductAndSum(int n) {
+
+    int product = 1;   // must start with 1
+    int sum = 0;
+
+    while (n > 0) {
+        int digit = n % 10;
+        product = product * digit;
+        sum = sum + digit;
+        n = n / 10;
+    }
+
+    return product - sum;
+}
