4782750e4cd3cf1ce038ebab227f850f4ef96409
Moulyagowda-19 <168709974+Moulyagowda-19@users.noreply.github.com>
2026-03-15 21:19:16 +0530

    Time: 25 ms (32.28%) | Memory: 42.6 MB (26.80%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#define SIZE 10000
+
+typedef struct {
+    int q[SIZE];
+    int front;
+    int rear;
+} RecentCounter;
+
+RecentCounter* recentCounterCreate() {
+    RecentCounter* obj = malloc(sizeof(RecentCounter));
+    obj->front = 0;
+    obj->rear = -1;
+    return obj;
+}
+
+int recentCounterPing(RecentCounter* obj, int t) {
+
+    obj->q[++obj->rear] = t;
+
+    while(obj->q[obj->front] < t - 3000){
+        obj->front++;
+    }
+
+    return obj->rear - obj->front + 1;
+}
+
+void recentCounterFree(RecentCounter* obj) {
+    free(obj);
+}
